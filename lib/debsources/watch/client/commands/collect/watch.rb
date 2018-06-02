c8877b7e0ae580260bd1054be30aaa4c6d45f52e
Kentaro Hayashi <kenhys@gmail.com>
2018-06-03 01:16:52 +0900

    Implement watch command

--- lib/debsources/watch/client/commands/collect/watch.rb
+++ lib/debsources/watch/client/commands/collect/watch.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../../command'
+require 'grn_mini'
+require 'open-uri'
 
 module Debsources
   module Watch
@@ -11,11 +13,29 @@ module Debsources
             def initialize(package=nil, options)
               @package = package
               @options = options
+              GrnMini::create_or_open("data/debian-watch.db")
+              @pkgs = GrnMini::Hash.new("Pkgs")
             end
 
             def execute(input: $stdin, output: $stdout)
-              # Command logic goes here ...
-              output.puts "OK"
+              if @package
+                update_watch_content(@package)
+              else
+                p @pkgs.keys
+              end
+            end
+
+            def update_watch_content(package)
+              raw_url = ""
+              open("https://sources.debian.org/api/src/#{package}/latest/debian/watch") do |response|
+                json = JSON.parse(response.read)
+                raw_url = json["raw_url"]
+              end
+              if raw_url
+                open("https://sources.debian.org/#{raw_url}") do |response|
+                  @pkgs[package] = {watch_content: response.read}
+                end
+              end
             end
           end
         end
