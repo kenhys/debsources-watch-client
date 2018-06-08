7e575c36742642fa3df27dad5f7c3e37b3665db0
Kentaro Hayashi <kenhys@gmail.com>
2018-06-08 23:54:00 +0900

    Fetch package version

--- lib/debsources/watch/client/commands/collect/pkglist.rb
+++ lib/debsources/watch/client/commands/collect/pkglist.rb
@@ -40,6 +40,19 @@ module Debsources
               end
               packages
             end
+
+            def fetch_package_version(package)
+              pkginfo_url = "https://sources.debian.org/api/src/#{package}"
+              p pkginfo_url
+              version = nil
+              open(pkginfo_url) do |request|
+                json = JSON.parse(request.read)
+                if json["versions"][0]["suites"].include?("sid")
+                  version = json["versions"][0]["version"]
+                end
+              end
+              version
+            end
           end
         end
       end
