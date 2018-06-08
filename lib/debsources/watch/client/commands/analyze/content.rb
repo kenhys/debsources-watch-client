b683c63a91e507375c7c8688618a574ca04fb96a
Kentaro Hayashi <kenhys@gmail.com>
2018-06-09 08:22:51 +0900

    Analyze hosting data

--- lib/debsources/watch/client/commands/analyze/content.rb
+++ lib/debsources/watch/client/commands/analyze/content.rb
@@ -13,8 +13,35 @@ module Debsources
             end
 
             def execute(input: $stdin, output: $stdout)
-              # Command logic goes here ...
-              output.puts "OK"
+              GrnMini::create_or_open("data/debian-watch.db")
+              @pkgs = GrnMini::Hash.new("Pkgs")
+              pkglist = []
+              @pkgs.each do |record|
+                if record.watch_content =~ /version=(\d)/
+                  #
+                  #p $1.to_i
+                  record.watch_version = $1.to_i
+                else
+                  #
+                end
+                if record.watch_content =~ /https?:\/\/(.+?)\//
+                  matched = $1.strip
+                  if matched.end_with?("sf.net") or matched.end_with?("sourceforge.net")
+                    p "sourceforge.net"
+                    record.watch_hosting = "sourceforge.net"
+                  else
+                    p matched
+                    record.watch_hosting = $1
+                  end
+                  #p "#{record._key} #{record.watch_version} #{$1}"
+                end
+              end
+
+              groups = GrnMini::Util::group_with_sort(@pkgs, "watch_version")
+              #p groups.size
+              groups = GrnMini::Util::group_with_sort(@pkgs, "watch_hosting")
+              #p groups.size
+              #p groups[0]
             end
           end
         end
