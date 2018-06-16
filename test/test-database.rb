9260e32f14ff4bc54505c24f438e73be39c85bc0
Kentaro Hayashi <kenhys@gmail.com>
2018-06-16 11:36:30 +0900

    Fix module names

--- test/test-database.rb
+++ test/test-database.rb
@@ -1,11 +1,11 @@
 class TestDatabase < Test::Unit::TestCase
 
   def test_init_help
-    output = `debsources-watch-client help init`
+    output = `debsources-watch-crawler help init`
     expected = <<-EOS
 Commands:
-  debsources-watch-client init database        # Initialize database file
-  debsources-watch-client init help [COMMAND]  # Describe subcommands or one specific subcommand
+  debsources-watch-crawler init database        # Initialize database file
+  debsources-watch-crawler init help [COMMAND]  # Describe subcommands or one specific subcommand
 
 EOS
     assert_equal(expected, output)
