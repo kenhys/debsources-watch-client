8baad64dc82f067245e379b70907abaa1f5821dc
Kentaro Hayashi <kenhys@gmail.com>
2018-06-23 17:16:45 +0900

    Implement to verify package

--- lib/debsources/watch/crawler/commands/verify/uscan.rb
+++ lib/debsources/watch/crawler/commands/verify/uscan.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../../command'
+require 'fileutils'
+require 'rexml/document'
 
 module Debsources
   module Watch
@@ -11,11 +13,55 @@ module Debsources
             def initialize(package, options)
               @package = package
               @options = options
+              path = "data/debian-watch.db"
+              unless File.exist?(path)
+                Groonga::Database.create(path: path)
+              else
+                Groonga::Database.open(path)
+              end
+              @dehs = Groonga["Dehs"]
             end
 
             def execute(input: $stdin, output: $stdout)
               # Command logic goes here ...
               output.puts "OK"
+              if @package
+                `apt source #{@package}`
+                target_dir = nil
+                Dir.glob("#{@package}*") do |dir|
+                  if File.directory?(dir)
+                    target_dir = dir
+                    doc = nil
+                    source = ""
+                    Dir.chdir(dir) do
+                      `dch --force-bad-version --newversion 0.0.0-1 "Test"`
+                      `dch --release "Test"`
+                      source=`perl #{ENV["USCAN_PATH"]} --dehs --no-download`
+                      doc = REXML::Document.new(source)
+                    end
+                    if doc
+                      #p source
+                      upstream_version = doc.elements["/dehs/upstream-version"].text
+                      upstream_url = doc.elements["/dehs/upstream-url"].text
+                      status = doc.elements["/dehs/status"].text
+                      if status == "newer package available"
+                        timestamp = Time.now
+                        @dehs.add(@package,
+                                  :package => @package,
+                                  :revised => source,
+                                  :upstream_version => upstream_version,
+                                  :upstream_url => upstream_url,
+                                  :status => status,
+                                  :updated_at => timestamp
+                                 )
+                      end
+                    end
+                    FileUtils.rm_rf(target_dir, :secure => true)
+                    FileUtils.rm_rf("#{@package}-0.0.0", :secure => true)
+                    FileUtils.rm_rf(Dir.glob("#{@package}_*"), :secure => true)
+                  end
+                end
+              end
             end
           end
         end
