561a7eb115278c52b3c742662379597c06825752
Kentaro Hayashi <kenhys@gmail.com>
2018-07-26 14:45:22 +0900

    Add helper method

--- lib/debsources/watch/crawler.rb
+++ lib/debsources/watch/crawler.rb
@@ -1,9 +1,17 @@
 require "debsources/watch/crawler/version"
+require "groonga"
 
 module Debsources
   module Watch
     module Crawler
-      # Your code goes here...
+
+      def create_or_open_database(path)
+        unless File.exist?(path)
+          Groonga::Database.create(path: path)
+        else
+          Groonga::Database.open(path)
+        end
+      end
     end
   end
 end
