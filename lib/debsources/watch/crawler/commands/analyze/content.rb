b4d8edb60636f184a2c12dc901699c34817a431c
Kentaro Hayashi <kenhys@gmail.com>
2018-07-29 23:22:25 +0900

    Fix syntax error

--- lib/debsources/watch/crawler/commands/analyze/content.rb
+++ lib/debsources/watch/crawler/commands/analyze/content.rb
@@ -15,7 +15,7 @@ module Debsources
               Debsources::Watch::Crawler.create_or_open_database(@config.database_path)
               @pkgs = Groonga["Pkgs"]
               root_dir = File.dirname(File.dirname(File.expand_path($0)))
-              @runner_path = File.join(root_dir),
+              @runner_path = File.join(root_dir,
                                        "bin/parse-watch-file")
             end
 
